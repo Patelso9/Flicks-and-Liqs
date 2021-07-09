34aafd71590318f2e5d55c3cb1da6542da0961a8
Princess <sonali.patel.pkg@gmail.com>
2021-07-09 12:09:31 -0400

    react setup works

---
+++ src/components/Cocktail/Cocktail.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Cocktail.css'
+
+const Cocktail = () => {
+
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Cocktail
