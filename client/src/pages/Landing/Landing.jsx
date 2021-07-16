8bc5df61d2aea878b3f981510e6ff8c94c57c387
Princess <sonali.patel.pkg@gmail.com>
2021-07-16 15:53:36 -0500

    udpate landing page and links

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { NavLink, Redirect } from 'react-router-dom';
 import './Landing.css';
 
 const Landing = () => {
+
+    const onSubmit = () => {
+        return <Redirect to="/login"/>
+    }
+
     return (
         <div>
             <h1 className='landing-logo'>Flicks&Liqs</h1>
-            <p>Are you stressed from coding?</p>
+            <p className='are-you'>Are you stressed from coding?</p>
+            <br />
             <div className='stress-options'>
-                <button className='stress-yes'>yes</button>
-                <p>or</p>
-                <button className='stress-yes'>yes</button>
+                <NavLink to="/login">
+                    <button className='stress-yes'>Yes</button>
+                    <h6>Log In</h6>
+                </NavLink>
+                <p className='or'>or</p>
+                <NavLink to="/signup">
+                    <button className='stress-yes'>Yes</button>
+                    <h6>Sign Up</h6>
+                </NavLink>
             </div>
         </div>
     )
